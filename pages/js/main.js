52f131e562188dd8717ffd478ca8ea81b159fcd8
F_G <18842685161@163.com>
2016-07-06 21:35:43 +0800

    contains all xx.js

--- pages/js/main.js
+++ pages/js/main.js
@@ -3,40 +3,54 @@ window.onload = function()
 	var tag = document.getElementById('header_tag');//标签导航
 	var tag_a = tag.getElementsByTagName("a");//标签导航下的a
 	var content = document.getElementById('content');//项目内容区域
-    var cons = getByClass(content,"con");//项目分块
+    var cons = getElementsByClassName("con");//项目分块
     var order = 0;//标记原先选项位置
 
-    console.log(tag_a.length);
+	tag_a[0].className = "active";
 	for (var i = 0; i < tag_a.length; i++) 
 	{
 		tag_a[i].value=i;
-		console.log(tag_a[i].value);
-		tag_a[i].onmouseover=function ()
+		tag_a[i].onmouseover=function () 
 		{
-		 	this.className="active";
-		 	console.log(this.value);
-		 	cons[this.value].style.display="block";
-		 	order=this.value;
-		 	console.log(order);
+			addClass(this,"active");
+			ChangeTabs(this.value);
 		}
-		tag_a[i].onmouseout=function()
+		function ChangeTabs (nowTab) 
 		{
-			tag_a[order].className="";
-			console.log(order);
-			cons[order].style.display="none";
+			removeClass(tag_a[order],"active");
+			cons[order].style.display = "none"; 
+			cons[nowTab].style.display = "block"; 
+			order=nowTab;
 		}
+		
 	}
+
 }
-function getByClass(oParent, sClassName)//通过类名获取
+
+function getElementsByClassName(n)//通过class名获取元素
 {
-    var aElm=oParent.getElementsByTagName('*');
-    var aArr=[];
-    for(var i=0; i<aElm.length; i++)
-    {
-        if(aElm[i].className==sClassName)
-        {
-            aArr.push(aElm[i]);
+    var classElements = [],allElements = document.getElementsByTagName('*');
+    for (var i=0; i< allElements.length; i++ )
+   {
+       if (allElements[i].className == n ) 
+       {
+           classElements[classElements.length] = allElements[i];
         }
-    }
-    return aArr;
+   }
+   return classElements;
 }
+function addClass(element, newClassName) //添加class名
+{
+    var oldClassName = element.className; //获取旧的样式类
+    element.className = oldClassName === "" ? newClassName : oldClassName + " " + newClassName;
+}
+function removeClass(element, oldClassName)//移除指定class名
+{
+    var originClassName = element.className; //获取原先的样式类
+    var pattern = new RegExp("\\b" + oldClassName + "\\b"); //使用构造函数构造动态的正则表达式
+    element.className = trim(originClassName.replace(pattern, ''));
+}
+function trim(str)//去除空格
+  {
+      return str.replace(/^\s+|\s+$/g,"");
+  }
